79e8ea1b43aebdb3eb6893fc1c6b2e9805933c04
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-02-25 18:52:40 +0000

    basic router setting

---
+++ controllers/property.controller.js
@@ -0,0 +1,16 @@
+import Property from "../mongodb/models/property.js";
+
+const getAllProperties = async (req, res) => {};
+
+const getPropertyDetail = async (req, res) => {};
+const createProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {};
+const deleteProperty = (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetail,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+};
